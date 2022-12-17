19996c9c76aa8dfbfbb414362cf33e3136bb1d7e
githubkieran <kieranharley1@gmail.com>
2022-12-17 21:00:54 +0000

    change for jenkins

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var www = http.createServer(handleRequest);
 www.listen(8080,function () {
     startTime = new Date();;
     host = process.env.HOSTNAME;
-    console.log ("Started At:",startTime, "| Running On: " ,host, "\n" );
+    console.log ("Started At:",startTime, "| Running On: " ,host, "\n" ); 
 });
 
  
